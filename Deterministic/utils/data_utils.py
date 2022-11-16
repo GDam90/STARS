ba4c933ba65d7130a98a789183a6a210b68774ce
GDam90 <guido.damely@gmail.com>
2022-11-16 17:31:51 +0100

    added dim_used

--- Deterministic/utils/data_utils.py
+++ Deterministic/utils/data_utils.py
@@ -194,6 +194,12 @@ def revert_output_format(poses, data_mean, data_std, dim_to_ignore, actions, one
     return poses_out_list
 
 
+dim_used = np.array([0, 1, 2, 3, 4, 5, 18, 19, 20, 33, 34, 35, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25,
+                    26, 27, 28, 29, 30, 31, 32, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+                    46, 47, 51, 52, 53, 54, 55, 56, 57, 58, 59, 63, 64, 65, 66, 67, 68,
+                    75, 76, 77, 78, 79, 80, 81, 82, 83, 87, 88, 89, 90, 91, 92])
+
+
 def readCSVasFloat(filename):
     """
     Borrowed from SRNN code. Reads a csv and returns a float matrix.
